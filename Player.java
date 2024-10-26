db6df5e8000e7355819550e2036bc0af7ed41b31
Girafficorn <gengjiexie123@gmail.com>
2024-10-26 17:02:09 -0400

    Created Constructors for Player Object and updated createPlayer and validatePlayer

--- Player.java
+++ Player.java
@@ -1,17 +1,45 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 public class Player {
     //Fields
     private int numFood;
     private String userName;
     private String password;
+    private Pet pet;
 
-    public static String createPlayer(String userName, String password) {
-        File f = new File("user.txt");
+    //Constructor
+    public Player() {
+        this.numFood = 0;
+        this.userName = null;
+        this.password = null;
+        this.pet = null;
+    }
+    public Player(String userName, String password, boolean newUser, Pet pet) {
+        this.userName = userName;
+        this.password = password;
+        this.pet = pet;
+        if (newUser) {
+            this.numFood = 0;
+        } else {
+            File f = new File("User.txt");
+            try {
+                BufferedReader bfr = new BufferedReader(new FileReader(f));
+                String line = bfr.readLine();
+                while (line != null) {
+                    if (line.substring(0, line.indexOf(",")).equals(userName)) {
+                        this.numFood = Integer.parseInt(line.substring(line.indexOf(",", line.indexOf(","))));
+                    }
+                }
+                bfr.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    //Methods
+    public static Player createPlayer(String userName, String password, String petName, String petType) {
+        File f = new File("User.txt");
         boolean exist = false;
         try {
             BufferedReader bfr = new BufferedReader(new FileReader(f));
@@ -28,34 +56,37 @@ public class Player {
             if (!exist) {
                 BufferedWriter bw = new BufferedWriter(new FileWriter(f, true));
                 bw.write(userName + "," + password);
-                bw.newLine();
                 bw.close();
-                return "User Successfully Created!";
+                return new Player(userName, password, true, new Pet(petName, petType));
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return "User Already Exist, Please Try Again!";
+        return new Player();
     }
 
-    public static boolean validatePlayer(String userName, String password) {
-        File f = new File("user.txt");
+    public static Player validatePlayer(String userName, String password) {
+        File f = new File("User.txt");
+        int index = 0;
         try {
             BufferedReader bfr = new BufferedReader(new FileReader(f));
             String line = bfr.readLine();
-
+            ArrayList<Pet> pets = new ArrayList<Pet>();
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream("Pets.txt"));
             while (line != null) {
+                pets.add((Pet) ois.readObject());
+                index++;
                 String[] info = line.split(",");
                 if (info[0].equals(userName)) {
                     if (info[1].equals(password)) {
-                        return true;
+                        return new Player(userName, password, false, pets.get(index));
                     }
                 }
             }
             bfr.close();
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        return false;
+        return new Player();
     }
 }
