2d118f845f6e910b90b12aee0dcfc1dff2d9515e
Selena Lin <lin1863@purdue.edu>
2024-10-26 15:02:52 -0400

    Update Pet.java
    Coded basic structure of class

--- Pet.java
+++ Pet.java
@@ -1,1 +1,49 @@
+public class Pet {
+  private String name;
+  private String type;
+  private int happiness;
 
+  public Pet(String name, String type) {
+    this.name = name;
+    this.type = type;
+    happiness = 50;
+  }
+
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public String getType() {
+    return type;
+  }
+  public void setType(String type) {
+    this.type = type;
+  }
+  public int getHappiness() {
+    return happiness;
+  }
+  public String statusCheck() {
+    if (happiness > 90) {
+      return name + " is very happy!";
+    } else if (happiness > 75) {
+      return name + " could be happier..";
+    } else if (happiness > 40) {
+      return name + " is not happy :( ";
+    } else {
+      return name + " is devastated.";
+    }
+  }
+  public void feed(int n) {
+    if (happiness + n < 100){
+      happiness += n;
+    }
+  }
+
+  public String toString() {
+    String s = "Name: " + name + "Type: " + type + "Happiness: " + happiness;
+    return s;
+  }
+  
+}
