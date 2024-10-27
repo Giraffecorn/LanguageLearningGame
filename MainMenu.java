c44dbfe182a3f147fc7073b8025a1f3a470900ef
Luk-bot2 <lhaggert@purdue.edu>
2024-10-26 20:16:04 -0400

    Finished main method and fixed food names

--- MainMenu.java
+++ MainMenu.java
@@ -9,8 +9,7 @@ public class MainMenu {
     WordMatch wordMatch = new WordMatch("Food.txt");
     String[] arr = {"1", "2", "3"};
     String[] ynArr = {"y", "n"};
-    input = repeatedPrompt("Welcome to Language Learner!\nLogin (1)\nCreate New User (2)\nExit (3)", arr, scan);
-    while(!(input.equals("3"))) {
+    while(true) {
       input = repeatedPrompt("Welcome to Language Learner!\nLogin (1)\nCreate New User (2)\nExit (3)", arr, scan);
       switch(input) {            
       case ("1"):
@@ -34,45 +33,78 @@ public class MainMenu {
          System.out.println("What type of pet do you want?");
          String newPetType = scan.nextLine();
          currentPlayer = Player.createPlayer(newUsername, newPassword, newPetName, newPetType);
+         break;
        case("3"):
          input = repeatedPrompt("Are you sure you would like to exit? (y/n)", ynArr, scan);
          if(input.equals("y")) {
-          input = "3";
+           return;
+         } else {
+           continue;
          }
-        break;
+
+      }
+      break;
+    }
+
+    String petName = currentPlayer.getPet().getName();
+    while(true) {
+      input = repeatedPrompt("Hello " + currentPlayer.getUserName() + ", what would you like to do?\n" + "Practice Mandarin (1)\n" + "Check on " + petName + " (2)\n" + "Exit (3)\n", arr, scan);
+      switch (input) {
+        case "1":
+          System.out.println("You will be prompted with foods in Mandarin, type the food name in English to earn food for your pet.\n" +
+                  "Enter exit at any time to return to the main menu.");
+          while (true) {
+            String word = wordMatch.getRandomWord();
+            System.out.println(word);
+            input = scan.nextLine();
+            if (input.equalsIgnoreCase("exit")) break;
+            if (wordMatch.match(word, input)) {
+              currentPlayer.addFood(1);
+              System.out.println("Correct! You have " + currentPlayer.getNumFood() + " food.");
+            } else {
+              System.out.println("Incorrect!");
+            }
+          }
+          break;
+        case "2":
+          while(true) {
+            input = repeatedPrompt(currentPlayer.getPet().statusCheck() + "\nFeed (1)\nExit (2)", new String[]{"1", "2"}, scan);
+            if(input.equals("1")) {
+              if(currentPlayer.getNumFood() > 0) {
+                while(true) {
+                  System.out.println("How much would you like to feed " + currentPlayer.getNumFood() + "?");
+                  input = scan.nextLine();
+                  try{
+                    int numToFeed = Integer.parseInt(input);
+                    currentPlayer.getPet().feed(numToFeed);
+                    currentPlayer.addFood(-numToFeed);
+                    System.out.print("You have " + currentPlayer.getNumFood() + " food.");
+                    break;
+                  } catch(NumberFormatException e) {
+                    System.out.println("Invalid input, please enter an integer");
+                    continue;
+                  }
+                }
+
+
+              } else {
+                System.out.println("You don't have enough food.");
+              }
+            } else if(input.equals("2")) {
+              break;
+            }
+          }
+          break;
+
+        case "3":
+          input = repeatedPrompt("Are you sure you would like to exit? (y/n)", ynArr, scan);
+          if (input.equals("y")) {
+            System.out.println("Goodbye!");
+            return;
+          }
+          break;
       }
     }
-    String petName = currentPlayer.getPet().getName();
-    input = repeatedPrompt("Hello, " + currentPlayer.getUserName() + ", what would you like to do?\n" + "Practice Mandarin (1)\n" + "Check on " + petName + "(2)\n" + "Exit (3)\n", arr, scan);
-    switch(input) {
-      case "1":
-        System.out.println("You will be prompted with foods in Mandarin, type the food name in English to earn food for your pet.\n" +
-                           "Enter exit at any time to return to the main menu.");
-        while(true) {
-          String word = wordMatch.getRandomWord();
-          System.out.println(word);
-          input = scan.nextLine();
-          if(input.toLowerCase().equals("exit")) break;
-          if(wordMatch.match(word, input)) {
-            currentPlayer.addFood(1);
-            System.out.println("Correct! You have " + currentPlayer.getNumFood() + " food.");
-          } else {
-            System.out.println("Incorrect!");
-          }
-        }
-        break;
-      case "2":
-        System.err.println("Helo");
-        break;
-        //TODO Check on pet
-      case "3":
-        input = repeatedPrompt("Are you sure you would like to exit? (y/n)", ynArr, scan);
-        if(input.equals("y")) {
-          System.out.println("Goodbye!");
-          return;
-        }
-        break;      
-  }
   }
 
   private static String repeatedPrompt(String prompt, String[] validInputs, Scanner scan) {
